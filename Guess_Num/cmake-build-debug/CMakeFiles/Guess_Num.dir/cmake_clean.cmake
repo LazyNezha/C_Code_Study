c40ebb742a9f56e1a45a328e75bc192871ef0574
lazynezha <noahxfyi@icloud.com>
2021-11-16 16:29:24 +0800

    Guess Number Game

---
+++ Guess_Num/cmake-build-debug/CMakeFiles/Guess_Num.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Guess_Num.dir/main.c.o"
+  "Guess_Num"
+  "Guess_Num.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Guess_Num.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
