54ffc7d572ec7174895946044e064016720a05c3
lazynezha <noahxfyi@icloud.com>
2021-11-23 12:05:32 +0800

    clion调用C静态库

---
+++ test/header/Add.h
@@ -0,0 +1,8 @@
+//
+// Created by 86175 on 2021/11/23.
+//
+
+#ifndef ADD_ADD_H
+#define ADD_ADD_H
+int Add(int, int);
+#endif //ADD_ADD_H
---
+++ test/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Add.h"
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = Add(a, b);
+
+    printf("%d\n", c);
+
+    return 0;
+}
