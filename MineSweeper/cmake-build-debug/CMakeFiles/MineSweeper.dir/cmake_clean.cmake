6eedd0b8d11db0f3c5b3a78909130a351115f476
lazynezha <noahxfyi@icloud.com>
2021-12-09 11:24:41 +0800

    扫雷游戏

---
+++ MineSweeper/cmake-build-debug/CMakeFiles/MineSweeper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MineSweeper.dir/game.c.o"
+  "CMakeFiles/MineSweeper.dir/main.c.o"
+  "MineSweeper"
+  "MineSweeper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MineSweeper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
